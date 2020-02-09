e4a72dd92cbef1ac9670ca36728bb36367c6a5d9
jintz <keisuke.igimi@gmail.com>
2020-02-09 21:44:01 +0900

    feat: フレームワークをRocketからActix webに変更する

--- src/controller/mod.rs
+++ src/controller/mod.rs
@@ -1,1 +1,28 @@
-pub mod index;
+use actix_web::{HttpRequest, HttpResponse};
+
+pub async fn hello() -> HttpResponse {
+  HttpResponse::Ok()
+    .content_type("plain/text")
+    .body("Hello world!")
+}
+
+pub async fn health() -> HttpResponse {
+  HttpResponse::Ok().content_type("plain/text").body("Ok.")
+}
+
+#[cfg(test)]
+mod test {
+  use super::*;
+
+  #[actix_rt::test]
+  async fn health_check() {
+    let res = health().await;
+    assert_eq!(res.status(), http::StatusCode::OK);
+  }
+
+  #[actix_rt::test]
+  async fn index() {
+    let res = health().await;
+    assert_eq!(res.status(), http::StatusCode::OK);
+  }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,39 +1,17 @@
-#![feature(proc_macro_hygiene, decl_macro)]
-
-#[macro_use]
-extern crate rocket;
-extern crate mysql;
+extern crate http;
 
 mod controller;
-mod repository;
 
-#[get("/health")]
-fn health() -> &'static str {
-  "OK."
-}
+use actix_web::{web, App, HttpServer};
 
-fn rocket() -> rocket::Rocket {
-  rocket::ignite().mount("/", routes![controller::index::index, health])
-}
-
-fn main() {
-  let _connection = repository::Connection::connect();
-
-  rocket().launch();
-}
-
-#[cfg(test)]
-mod test {
-  use super::rocket;
-  use rocket::http::Status;
-  use rocket::local::Client;
-
-  #[test]
-  fn health_check() {
-    let client = Client::new(rocket()).expect("valid rocket instance");
-    let mut health = client.get("/health").dispatch();
-    assert_eq!(health.status(), Status::Ok);
-    assert_eq!(health.body_string(), Some("OK.".into()));
-    assert_ne!(health.body_string(), Some("NG.".into()));
-  }
+#[actix_rt::main]
+async fn main() -> std::io::Result<()> {
+  HttpServer::new(|| {
+    App::new()
+      .route("/", web::get().to(controller::hello))
+      .route("/health", web::get().to(controller::health))
+  })
+  .bind("127.0.0.1:8000")?
+  .run()
+  .await
 }
