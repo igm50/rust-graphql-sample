9cc12f0255f87e34f71eba12a42d364d3aa4e2c0
jintz <keisuke.igimi@gmail.com>
2020-02-09 10:38:54 +0900

    test: ヘルスチェックとindexエンドポイントのテストを記述する

--- src/controller/index.rs
+++ src/controller/index.rs
@@ -1,4 +1,15 @@
 #[get("/")]
 pub fn index() -> &'static str {
-    "Hello, world!"
+  "Hello, world!"
+}
+
+#[cfg(test)]
+mod test {
+  use super::index;
+
+  #[test]
+  fn index_test() {
+    assert_eq!(index(), "Hello, world!");
+    assert_ne!(index(), "Good bye, world!");
+  }
 }
--- src/main.rs
+++ src/main.rs
@@ -7,10 +7,33 @@ extern crate mysql;
 mod controller;
 mod repository;
 
-fn main() {
-    let _connection = repository::Connection::connect();
+#[get("/health")]
+fn health() -> &'static str {
+  "OK."
+}
 
-    rocket::ignite()
-        .mount("/", routes![controller::index::index])
-        .launch();
+fn rocket() -> rocket::Rocket {
+  rocket::ignite().mount("/", routes![controller::index::index, health])
+}
+
+fn main() {
+  let _connection = repository::Connection::connect();
+
+  rocket().launch();
+}
+
+#[cfg(test)]
+mod test {
+  use super::rocket;
+  use rocket::http::Status;
+  use rocket::local::Client;
+
+  #[test]
+  fn health_check() {
+    let client = Client::new(rocket()).expect("valid rocket instance");
+    let mut health = client.get("/health").dispatch();
+    assert_eq!(health.status(), Status::Ok);
+    assert_eq!(health.body_string(), Some("OK.".into()));
+    assert_ne!(health.body_string(), Some("NG.".into()));
+  }
 }
