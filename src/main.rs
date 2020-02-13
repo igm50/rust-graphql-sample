7e39ce74ca5cf5daf55e1a8146cb7660bd93d8b7
jintz <keisuke.igimi@gmail.com>
2020-02-13 09:38:30 +0900

    test: インテグレーションテストを追加する

--- src/main.rs
+++ src/main.rs
@@ -13,3 +13,29 @@ async fn main() -> std::io::Result<()> {
   .run()
   .await
 }
+
+// integration tests
+#[cfg(test)]
+mod tests {
+  use crate::controller::*;
+  use actix_web::{test, web, App};
+
+  #[actix_rt::test]
+  async fn test_health() {
+    let path = "/";
+    let mut app = test::init_service(App::new().route(path, web::get().to(health))).await;
+    let req = test::TestRequest::get()
+      .uri(path)
+      .header("content-type", "text/plain")
+      .to_request();
+    let res = test::call_service(&mut app, req).await;
+    assert!(res.status().is_success());
+
+    let req = test::TestRequest::post()
+      .uri(path)
+      .header("content-type", "text/plain")
+      .to_request();
+    let res = test::call_service(&mut app, req).await;
+    assert!(res.status().is_client_error());
+  }
+}
