8a88c835ac5505fd6d94e39da1846b91dda7dbcc
jintz <keisuke.igimi@gmail.com>
2020-02-20 17:55:33 +0900

    refactor: パスの設定を切り出す

--- src/main.rs
+++ src/main.rs
@@ -4,41 +4,41 @@ mod schema;
 
 use actix_web::{web, App, HttpServer};
 
+fn config(cfg: &mut web::ServiceConfig) {
+  cfg
+    .route("/", web::get().to(controller::health))
+    .route("/graphiql", web::get().to(controller::graphiql))
+    .route("/graphql", web::post().to(controller::graphql));
+}
+
 #[actix_rt::main]
 async fn main() -> std::io::Result<()> {
   let schema = std::sync::Arc::new(schema::create_schema());
 
-  HttpServer::new(move || {
-    App::new()
-      .data(schema.clone())
-      .route("/", web::get().to(controller::health))
-      .route("/graphiql", web::get().to(controller::graphiql))
-      .route("/graphql", web::post().to(controller::graphql))
-  })
-  .bind("0.0.0.0:8000")?
-  .run()
-  .await
+  HttpServer::new(move || App::new().data(schema.clone()).configure(config))
+    .bind("0.0.0.0:8000")?
+    .run()
+    .await
 }
 
 // integration tests
 #[cfg(test)]
 mod tests {
-  use crate::controller::*;
-  use actix_web::{test, web, App};
+  use super::config;
+  use actix_web::{test, App};
 
   #[actix_rt::test]
   async fn test_health() {
-    let path = "/";
-    let mut app = test::init_service(App::new().route(path, web::get().to(health))).await;
+    let mut app = test::init_service(App::new().configure(config)).await;
     let req = test::TestRequest::get()
-      .uri(path)
+      .uri("/")
       .header("content-type", "text/plain")
       .to_request();
     let res = test::call_service(&mut app, req).await;
     assert!(res.status().is_success());
 
     let req = test::TestRequest::post()
-      .uri(path)
+      .uri("/")
       .header("content-type", "text/plain")
       .to_request();
     let res = test::call_service(&mut app, req).await;
