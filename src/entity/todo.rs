9268d2fe42000dddd7163acda49c8ba859fcc575
jintz <keisuke.igimi@gmail.com>
2020-02-26 17:25:26 +0900

    feat: ToDoクエリのダミーを作成する

--- src/entity/todo.rs
+++ src/entity/todo.rs
@@ -1,3 +1,4 @@
+use juniper::graphql_object;
 use uuid::Uuid;
 
 #[derive(Debug)]
@@ -6,10 +7,40 @@ pub struct ToDo {
   text: String,
 }
 
+impl ToDo {
+  pub fn new(id: Uuid, text: String) -> ToDo {
+    ToDo { id: id, text: text }
+  }
+
+  pub fn id(&self) -> Uuid {
+    self.id
+  }
+
+  pub fn text(&self) -> String {
+    self.text.clone()
+  }
+}
+
 impl PartialEq for ToDo {
   fn eq(&self, other: &Self) -> bool {
     self.id == other.id
   }
+}
+
+graphql_object!(ToDo: () |&self| {
+  description: "Task to do."
+
+  field id() -> String {
+    self.id.to_string()
+  }
+
+  field text() -> &String {
+    &self.text
+  }
+});
+
+pub trait Repository<E> {
+  fn create(&self, todo: ToDo) -> Result<ToDo, E>;
 }
 
 #[cfg(test)]
@@ -18,7 +49,35 @@ mod test {
   use uuid::Uuid;
 
   #[test]
-  fn partial_eq() {
+  fn test_id() {
+    let todo = ToDo {
+      id: Uuid::parse_str("97103fab-1e50-36b7-0c03-0938362b0809").unwrap(),
+      text: String::from("sample"),
+    };
+
+    assert_eq!(
+      todo.id().to_string(),
+      "97103fab-1e50-36b7-0c03-0938362b0809"
+    );
+    assert_ne!(
+      todo.id().to_string(),
+      "aaaaaaaa-1e50-36b7-0c03-0938362b0809"
+    );
+  }
+
+  #[test]
+  fn test_text() {
+    let todo = ToDo {
+      id: Uuid::parse_str("97103fab-1e50-36b7-0c03-0938362b0809").unwrap(),
+      text: String::from("sample"),
+    };
+
+    assert_eq!(todo.text(), "sample");
+    assert_ne!(todo.text(), "not equal");
+  }
+
+  #[test]
+  fn test_partial_eq() {
     let id = Uuid::parse_str("97103fab-1e50-36b7-0c03-0938362b0809").unwrap();
 
     assert_eq!(
@@ -29,6 +88,17 @@ mod test {
       ToDo {
         id: id,
         text: String::from("other")
+      }
+    );
+
+    assert_ne!(
+      ToDo {
+        id: id,
+        text: String::from("one")
+      },
+      ToDo {
+        id: Uuid::parse_str("aaaaaaaa-1e50-36b7-0c03-0938362b0809").unwrap(),
+        text: String::from("one")
       }
     );
   }
--- src/schema.rs
+++ src/schema.rs
@@ -1,23 +1,34 @@
 use juniper::FieldResult;
 use juniper::RootNode;
+use uuid::Uuid;
 
-use juniper::GraphQLObject;
-
-#[derive(GraphQLObject)]
-#[graphql(description = "Sample query")]
-struct Sample {
-  name: String,
-}
+use crate::entity::todo::ToDo;
 
 pub struct QueryRoot;
 pub struct MutationRoot;
 
 #[juniper::object]
 impl QueryRoot {
-  fn sample() -> FieldResult<Sample> {
-    Ok(Sample {
-      name: String::from("sample query"),
-    })
+  fn todos() -> FieldResult<Vec<ToDo>> {
+    let result = vec![
+      ToDo::new(
+        Uuid::parse_str("87103fab-1e50-36b7-0c03-0938362b0809").unwrap(),
+        String::from("sample A"),
+      ),
+      ToDo::new(
+        Uuid::parse_str("97103fab-2e50-46b7-1c03-1938362b0809").unwrap(),
+        String::from("sample B"),
+      ),
+    ];
+
+    Ok(result)
+  }
+
+  fn todo(text: String) -> FieldResult<ToDo> {
+    Ok(ToDo::new(
+      Uuid::parse_str("97103fab-1e50-36b7-0c03-0938362b0809").unwrap(),
+      text,
+    ))
   }
 }
 
